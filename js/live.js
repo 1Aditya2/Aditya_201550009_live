fe119052c68140cb66db594c8ed47476809a13dc
1Aditya2 <yadiyadav515@gmail.com>
2022-07-11 22:20:41 +0530

    Final Changes

---
+++ js/live.js
@@ -0,0 +1,21 @@
+
+
+function data(){
+    let name=document.createElement("h2")
+    let rollno=document.createElement("h2")
+    let uni=document.createElement("h2")
+    let date=document.createElement("h2")
+
+    name.innerText="Adity Yadav"
+    rollno.innerText="201550009"
+    uni.innerText="GLA University,Mathura"
+    var today=new Date()
+    date.innerText=`${today.getDate()}-${today.getMonth()}-${today.getFullYear()}`
+
+    document.querySelector("footer").appendChild(name)
+    document.querySelector("footer").appendChild(rollno)
+    document.querySelector("footer").appendChild(uni)
+    document.querySelector("footer").appendChild(date)
+}
+data()
+
